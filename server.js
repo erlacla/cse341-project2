70a25df153dc46b9406e4d54529147421304e6cb
Erin Engler <erlacla@msn.com>
2023-06-02 16:17:48 -0700

    Adding auth0 for log in and log out

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
@@ -27,4 +30,21 @@ mongodb.initDb((err) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+});
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
