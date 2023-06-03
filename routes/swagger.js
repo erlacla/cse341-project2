6d1744734346c0c797dacc4fee8e5a869e395158
Erin Engler <erlacla@msn.com>
2023-06-03 14:56:38 -0700

    adding auth0 to swagger.js

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,9 @@
 const routes = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+const { requiresAuth } = require('express-openid-connect');
 
 routes.use('/api-docs', swaggerUi.serve);
-routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+routes.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDocument));
 
 module.exports = routes;
